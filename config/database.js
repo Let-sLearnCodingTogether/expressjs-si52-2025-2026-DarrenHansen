f037c8f124f4a6205c761c4dcb97dfe4aa6de751
Darren Hansen <darrenhansen_2327240163@mhs.mdp.ac.id>
2025-10-18 18:22:21 +0700

    reposi

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 export const database = async () => {
     try {
         console.log("Starting Connection to Mongodb")
-        const response = await mongoose.connect("mongodb://127.0.0.1:27017/Owen?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
+        const response = await mongoose.connect("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.8")
         console.log (`Connected successfully to mongodb at ; ${response.connection.host}`);
         
     } catch (error) {
